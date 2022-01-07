22301f1af8c9c0d25a1f0d1717a3d411e3b28bb0
Manielo <manielojeanpierre1@gmail.com>
2022-01-07 18:24:31 -0500

    Initial commit

---
+++ app/src/main/java/com/example/instagram/ParseApplication.java
@@ -0,0 +1,18 @@
+package com.example.instagram;
+
+import android.app.Application;
+
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("LXfw0IRnQ46p39Cv0Jc69a5L3MFF01kyGjq8iJZm")
+                .clientKey("VN3HUFQ2bZuDtpzvkc3HiEqLBfNYgeGMN5KeoPs4")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
